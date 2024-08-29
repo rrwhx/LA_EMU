22013504285f0851377d5e494f4a8031337222e6
ghy <2247883756@qq.com>
2024-08-30 00:46:01 +0800

    feat(plugins)simpoint.cc:deal first interval & add id in ckpt name

--- plugins/simpoint.cc
+++ plugins/simpoint.cc
@@ -15,9 +15,22 @@ using namespace std;
 bool begin_after_ibar0x40;
 static uint64_t interval_size = 1000000;
 map<uint64_t, double> icount2weight;
+map<uint64_t, uint64_t> icount2id;
 uint64_t icount;
 bool begin_collect;
 string ckpt_save_path;
+
+bool dump_first_interval;
+double first_interval_weight;
+uint64_t first_interval_id;
+
+static void check_exit() {
+    if (icount2weight.empty() && !dump_first_interval) {
+        fprintf(stderr, "all checkpoints have been dumped\n");
+        fflush(NULL);
+        exit(0);
+    }
+}
 
 void my_emu_insn_before(void* env, uint64_t pc, uint32_t insn) {
     // begin collect after exec ibar 0x40
@@ -29,15 +42,18 @@ void my_emu_insn_before(void* env, uint64_t pc, uint32_t insn) {
         }
     }
 
+    if (dump_first_interval && icount == 0) {
+        fprintf(stderr, "simpoint dump first interval,weight=%lf,id=%ld\n", first_interval_weight, first_interval_id);
+        la_emu_save_checkpoint(env, (char*)(ckpt_save_path  + "/" + "w" + to_string(first_interval_weight) + "_" + to_string(first_interval_id)).c_str());
+        dump_first_interval = false;
+        check_exit();
+    }
+
     if (icount2weight.count(icount)) {
-        fprintf(stderr, "simpoint dump checkpoint at icount=%ld,weight=%lf\n", icount, icount2weight[icount]);
-        la_emu_save_checkpoint(env, (char*)(ckpt_save_path  + "/" + "w" + to_string(icount2weight[icount])).c_str());
+        fprintf(stderr, "simpoint dump checkpoint at icount=%ld,weight=%lf,id=%ld\n", icount, icount2weight[icount], icount2id[icount]);
+        la_emu_save_checkpoint(env, (char*)(ckpt_save_path  + "/" + "w" + to_string(icount2weight[icount]) + "_" + to_string(icount2id[icount])).c_str());
         icount2weight.erase(icount);
-        if (icount2weight.empty()) {
-            fprintf(stderr, "all checkpoints have been dumped\n");
-            fflush(NULL);
-            exit(0);
-        }
+        check_exit();
     }
 
     icount++;
@@ -75,18 +91,21 @@ extern "C" la_emu_plugin_ops* la_emu_plugin_install(const char* arg) {
 
     uint64_t interval_num;
     double weight;
+    uint64_t simpoints_id, weights_id;
 
-    while (fscanf(simpoints_file, "%ld%*f", &interval_num) == 1) {
-        lsassert(fscanf(weights_file, "%lf%*f", &weight) == 1);
+    while (fscanf(simpoints_file, "%ld %ld", &interval_num, &simpoints_id) == 2) {
+        lsassert(fscanf(weights_file, "%lf %ld", &weight, &weights_id) == 2);
+        lsassert(simpoints_id == weights_id);
         if (interval_num == 0) {
-            fprintf(stderr, "warn:simpoint select first interval,weight=%lf,just discard.\n", weight);
-            if (weight > 0.1) {
-                fprintf(stderr, "can not ignore the first interval!\n");
-                exit(1);
-            }
+            // if the first interval is selected, no warmup is performed
+            fprintf(stderr, "warn:simpoint select first interval,weight=%lf\n", weight);
+            dump_first_interval = true;
+            first_interval_weight = weight;
+            first_interval_id = simpoints_id;
         } else {
             interval_num -= 1; // warmup inst num : simpoint inst num = 1 : 1
             icount2weight[interval_num * interval_size] = weight;
+            icount2id[interval_num * interval_size] = simpoints_id;
         }
     }
     fclose(simpoints_file);
