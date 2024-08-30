35eb88b71703b857f83923c48da193bcbdb8ee95
ghy <2247883756@qq.com>
2024-08-30 19:36:47 +0800

    feat(checkpoint):support qemu format checkpoint save & restore
    restore ckpt: use --ckpt-mem to specify mem.bin, use --ckpt-cpu to
    specify cpu.S
    
    save ckpt: in CLI mode: qckpt <name>
    
    fix(checkpoint):fix stable_timer

--- checkpoint.c
+++ checkpoint.c
@@ -16,6 +16,8 @@
 #if !defined(CONFIG_USER_ONLY)
 
 #define __4KB 0x1000
+#define PAGE_SIZE __4KB
+#define PAGE_MASK 0XFFFUL
 
 #define LOWMEM_BEGIN 0ul
 #define LOWMEM_SIZE 0x10000000ul
@@ -24,6 +26,8 @@
 
 static uint8_t zero4k[__4KB] __attribute__ ((aligned (__4KB)));
 extern char* ram;
+extern uint64_t ram_size;
+extern const char* const csrnames[];
 
 static void loongarch_cpu_dump_state(CPULoongArchState *env, FILE *f);
 static void loongarch_cpu_restore_state(CPULoongArchState *env, FILE *f);
@@ -153,9 +157,9 @@ void restore_checkpoint(CPULoongArchState *env, char* image_dir)
     printf("restore from icount=%ld\n", env->icount);
     loongarch_cpu_restore_state(env, reg_file);
 
-    if (env->CSR_CNTC < 0) {
-        env->CSR_CNTC = 0;
-    }
+    // if (env->CSR_CNTC < 0) {
+    //     env->CSR_CNTC = 0;
+    // }
     env->timer_counter = env->CSR_TVAL;
     env->CSR_TICLR = 0;
 }
@@ -355,12 +359,421 @@ static void loongarch_cpu_restore_state(CPULoongArchState *env, FILE* f)
     }
 }
 
+
+// --------- support qemu format checkpoint save/restore ---------
+
+static uint32_t xorsum32(const uint8_t *data, uint64_t size)
+{
+    uint32_t sum = 0;
+    uint64_t i;
+
+    for (i=0; i<size; i+=4) {
+        sum ^= *(const uint32_t *)(data + i);
+    }
+
+    return sum;
+}
+
+static void restore_memory_qemu_format(CPULoongArchState *env, char* mem_path) {
+     /*
+       file format:
+
+       offset       field       size
+       ------------------------------------
+       0x0          mem_size    8
+       0x8          data_off    4
+       0xc          xorsum      4
+       0x10         page_bmp    mem_size/4K/8
+       data_off     mem_data    mem_size
+
+       */
+    uint64_t mem_size, page_bmp_size, mem_page_num;
+    uint32_t data_off, sum, cal_sum;
+    uint8_t *page_bmp;
+    uint8_t* p;
+
+    FILE* f = fopen_nofail(mem_path, "rb");
+    lsassert(fread(&mem_size, sizeof(mem_size), 1, f) == 1);
+    lsassert(fread(&data_off, sizeof(data_off), 1, f) == 1);
+    lsassert(fread(&sum, sizeof(sum), 1, f) == 1);
+    mem_page_num = (mem_size  + PAGE_SIZE - 1) / PAGE_SIZE;
+    page_bmp_size = (mem_page_num + 7) / 8;
+    page_bmp = malloc(page_bmp_size);
+    lsassert(fread(page_bmp, page_bmp_size, 1, f) == 1);
+
+    fseek(f, data_off, SEEK_SET);
+
+    p = (uint8_t*)ram;
+    cal_sum = 0;
+    uint64_t restore_mem_size_bytes = 0;
+    for (int i = 0; i < mem_page_num; i++) {
+        int dirty = page_bmp[i / 8] & (1 << (i % 8));
+        if (dirty) {
+            lsassert(fread(p, PAGE_SIZE, 1, f) == 1);
+        }
+        cal_sum ^= xorsum32(p, PAGE_SIZE);
+        p += PAGE_SIZE;
+        restore_mem_size_bytes += PAGE_SIZE;
+        if (restore_mem_size_bytes == LOWMEM_SIZE) {
+            p = (uint8_t*)ram + HIGHMEM_BEGIN;
+        }
+    }
+    if (sum != cal_sum) {
+        fprintf(stderr, "warn:xorsum mismatch (restored: %08x original: %08x)\n", cal_sum, sum);
+    }
+    // lsassertm(sum == cal_sum, "xorsum mismatch (restored: %08x original: %08x)\n", cal_sum, sum);
+
+    fclose(f);
+    free(page_bmp);
+}
+
+static void save_memory_qemu_format(CPULoongArchState *env, char* mem_path) {
+    /*
+       file format:
+
+       offset       field       size
+       ------------------------------------
+       0x0          mem_size    8
+       0x8          data_off    4
+       0xc          xorsum      4
+       0x10         page_bmp    mem_size/4K/8
+       data_off     mem_data    mem_size
+
+       */
+
+    FILE* f = fopen_nofail(mem_path, "wb");
+    uint64_t mem_size = ram_size;
+    uint64_t mem_page_num = (mem_size  + PAGE_SIZE - 1) / PAGE_SIZE;
+    uint64_t page_bmp_size = (mem_page_num + 7) / 8;
+    uint8_t* ram_ptr = (uint8_t*)ram;
+
+    uint32_t header_size, data_off, padding;
+    uint32_t sum = 0;
+    uint8_t *page_bmp;
+
+    header_size = 16 + page_bmp_size;
+    data_off = padding = (header_size + PAGE_SIZE - 1) & ~PAGE_MASK;
+    padding -= header_size;
+
+    page_bmp = malloc(page_bmp_size);
+    memset(page_bmp, 0, page_bmp_size);
+
+    fseek(f, data_off, SEEK_SET);
+    lsassert((LOWMEM_SIZE % PAGE_SIZE) == 0);
+    for (uint64_t addr = 0; addr < LOWMEM_SIZE; addr += PAGE_SIZE) {
+        int dirty = memcmp(ram_ptr + addr, zero4k, PAGE_SIZE);
+        if (dirty) {
+            uint64_t page = addr / PAGE_SIZE;
+            page_bmp[page / 8] |= 1 << (page % 8);
+            lsassert(fwrite(ram_ptr + addr, PAGE_SIZE, 1, f) == 1);
+        }
+        sum ^= xorsum32(ram_ptr + addr, PAGE_SIZE);
+    }
+
+    for (uint64_t addr = HIGHMEM_BEGIN; addr < (HIGHMEM_BEGIN + mem_size - LOWMEM_SIZE); addr += PAGE_SIZE) {
+        int dirty = memcmp(ram_ptr + addr, zero4k, PAGE_SIZE);
+        if (dirty) {
+            uint64_t page = (addr - HIGHMEM_BEGIN + LOWMEM_SIZE) / PAGE_SIZE;
+            page_bmp[page / 8] |= 1 << (page % 8);
+            lsassert(fwrite(ram_ptr + addr, PAGE_SIZE, 1, f) == 1);
+        }
+        sum ^= xorsum32(ram_ptr + addr, PAGE_SIZE);
+    }
+
+    fseek(f, 0, SEEK_SET);
+    fwrite(&mem_size, sizeof(mem_size), 1, f);
+    fwrite(&data_off, sizeof(data_off), 1, f);
+    fwrite(&sum, sizeof(sum), 1, f);
+    fwrite(page_bmp, page_bmp_size, 1, f);
+    fwrite(zero4k, padding, 1, f);
+
+    fclose(f);
+    free(page_bmp);
+}
+
+static int check_and_replace_csr_save_dmw(char *str) {
+    char new_str[64];
+    int index = 0;
+    int i = 0;
+
+    // check "SAVEn" and convert to SAVE(n)
+    if (strncmp(str, "SAVE", 4) == 0 && isdigit(str[4])) {
+        index += snprintf(new_str, sizeof(new_str), "SAVE(");
+        while (isdigit(str[4 + i])) {
+            new_str[index++] = str[4 + i++];
+        }
+        new_str[index++] = ')';
+        new_str[index] = '\0';
+        strncpy(str, new_str, 64);
+        return 1;
+    }
+
+    // check "DMWn" and convert to DMW(n)
+    if (strncmp(str, "DMW", 3) == 0 && isdigit(str[3])) {
+        index += snprintf(new_str, sizeof(new_str), "DMW(");
+        while (isdigit(str[3 + i])) {
+            new_str[index++] = str[3 + i++];
+        }
+        new_str[index++] = ')';
+        new_str[index] = '\0';
+        strncpy(str, new_str, 64);
+        return 1;
+    }
+
+    return 0;
+}
+
+static int64_t csrname2idx(char* full_csr_name) {
+    check_and_replace_csr_save_dmw(full_csr_name);
+    for (uint64_t idx = 0; idx < LOONGARCH_CSR_MAXADDR + 1; idx++) {
+        if (csrnames[idx] && (strcmp(csrnames[idx], full_csr_name) == 0)) {
+            return idx;
+        }
+    }
+    fprintf(stderr, "can not find csr name=%s\n", full_csr_name);
+    return -1;
+}
+
+static void restore_cpu_qemu_format(CPULoongArchState *env, char* cpu_path) {
+    FILE* f = fopen_nofail(cpu_path, "r");
+    char line[1024];
+    uint64_t value;
+    // discard unuseful lines
+    long int position = 0;
+    while (fgets(line, sizeof(line), f)) {
+        if (strncmp(line, ".dword", 6) == 0) {
+            fseek(f, position, SEEK_SET);
+            break;
+        }
+        position = ftell(f);
+    }
+
+    // restore pc
+    lsassert(fgets(line, sizeof(line), f));
+    lsassert(sscanf(line, ".dword %lx", &(env->pc)) == 1);
+    // restore gpr
+    for (int i = 0; i < 32; i++) {
+        lsassert(fgets(line, sizeof(line), f));
+        lsassert(sscanf(line, ".dword %lx", &(env->gpr[i])) == 1);
+    }
+    // restore fcsr0
+    lsassert(fgets(line, sizeof(line), f));
+    lsassert(sscanf(line, ".dword %lx", &value) == 1);
+    env->fcsr0 = value;
+    // restore fcc
+    for (int i = 0; i < 8; i++) {
+        lsassert(fgets(line, sizeof(line), f));
+        lsassert(sscanf(line, ".dword %lx", &value) == 1);
+        env->cf[i] = value;
+    }
+    // restore fpr
+    for (int i = 0; i < 32; i++) {
+        for (int j = 0; j < 4; j++) {
+            lsassert(fgets(line, sizeof(line), f));
+            lsassert(sscanf(line, ".dword %lx", &(env->fpr[i].vreg.UD[j])) == 1);
+        }
+    }
+    // restore csr & timer
+    char csr_name[1024];
+    while (fgets(line, sizeof(line), f)) {
+        if (sscanf(line, ".dword %lx # CSR_%255s", &value, csr_name) == 2) {
+            uint64_t csr_idx = csrname2idx(csr_name);
+            if (csr_idx != -1) {
+                uint64_t* csr_ptr = get_csr_ptr(env, csr_idx);
+                *csr_ptr = value;
+            }
+        } else if (sscanf(line, ".dword %lx # TIMER", &value) == 1) {
+            // LA_EMU use icount + CSR_CNTC as stable_timer "read out" value
+            env->icount = value - env->CSR_CNTC;
+            fprintf(stderr, "restore icount(stable_timer) to %ld\n", env->icount);
+        }
+    }
+}
+
+static void save_cpu_qemu_format(CPULoongArchState *env, char* cpu_path) {
+    FILE* f = fopen_nofail(cpu_path, "w");
+
+    int cpu_index = 0; // TODO : support SMP
+    fprintf(f, ".section .rodata\n\n");
+    fprintf(f, ".global core_state_%d\n", cpu_index);
+    fprintf(f, "core_state_%d:\n", cpu_index);
+
+    int idx = 0;
+
+    /* force the next sc to fail as in helper_ertn */
+    env->lladdr = 1;
+    fprintf(f, ".dword  0x%016" PRIx64 " # PC 0x%x\n", env->pc, 8 * idx++);
+
+    for (int i = 0; i < 32; i++) {
+        fprintf(f, ".dword  0x%016" PRIx64 " # GPR_%d 0x%x\n",
+                env->gpr[i], i, 8*idx++);
+    }
+
+    fprintf(f, ".dword  0x%016x # FCSR_0 0x%x\n", env->fcsr0, 8*idx++);
+
+    for (int i = 0; i < 8; i++) {
+        fprintf(f, ".dword  0x%016x # FCC_%d 0x%x\n",
+                env->cf[i] ? 1 : 0, i, 8*idx++);
+    }
+
+    for (int i = 0; i < 32; i++) {
+        for (int j = 0; j < 4; j++) {
+            fprintf(f, ".dword  0x%016" PRIx64 " # FPR_%d_D_%d 0x%x\n",
+                    env->fpr[i].vreg.UD[j], i, j, 8*idx++);
+        }
+    }
+
+#define PRINT_CSR(csr) \
+        fprintf(f, ".dword  0x%016" PRIx64 " # " #csr " 0x%x\n", \
+                env->csr, 8*idx++)
+
+#define PRINT_CSR_N(csr, n) \
+        fprintf(f, ".dword  0x%016" PRIx64 " # " #csr #n " 0x%x\n", \
+                env->csr[n], 8*idx++)
+
+    PRINT_CSR(CSR_CRMD);
+    PRINT_CSR(CSR_PRMD);
+    PRINT_CSR(CSR_EUEN);
+    PRINT_CSR(CSR_MISC);
+    PRINT_CSR(CSR_ECFG);
+    PRINT_CSR(CSR_ESTAT);
+    PRINT_CSR(CSR_ERA);
+    PRINT_CSR(CSR_BADV);
+    PRINT_CSR(CSR_BADI);
+
+    PRINT_CSR(CSR_EENTRY);
+
+    PRINT_CSR(CSR_TLBIDX);
+    PRINT_CSR(CSR_TLBEHI);
+    PRINT_CSR(CSR_TLBELO0);
+    PRINT_CSR(CSR_TLBELO1);
+
+    PRINT_CSR(CSR_ASID);
+    PRINT_CSR(CSR_PGDL);
+    PRINT_CSR(CSR_PGDH);
+    fprintf(f, ".dword  0x%016" PRIx64 " # CSR_PGD 0x%x\n", \
+                helper_csrrd_pgd(env) , 8*idx++);
+    PRINT_CSR(CSR_PWCL);
+    PRINT_CSR(CSR_PWCH);
+    PRINT_CSR(CSR_STLBPS);
+    PRINT_CSR(CSR_RVACFG);
+    PRINT_CSR(CSR_CPUID);
+    PRINT_CSR(CSR_PRCFG1);
+    PRINT_CSR(CSR_PRCFG2);
+    PRINT_CSR(CSR_PRCFG3);
+
+    PRINT_CSR_N(CSR_SAVE, 0);
+    PRINT_CSR_N(CSR_SAVE, 1);
+    PRINT_CSR_N(CSR_SAVE, 2);
+    PRINT_CSR_N(CSR_SAVE, 3);
+    PRINT_CSR_N(CSR_SAVE, 4);
+    PRINT_CSR_N(CSR_SAVE, 5);
+    PRINT_CSR_N(CSR_SAVE, 6);
+    PRINT_CSR_N(CSR_SAVE, 7);
+    PRINT_CSR_N(CSR_SAVE, 8);
+    PRINT_CSR_N(CSR_SAVE, 9);
+    PRINT_CSR_N(CSR_SAVE, 10);
+    PRINT_CSR_N(CSR_SAVE, 11);
+    PRINT_CSR_N(CSR_SAVE, 12);
+    PRINT_CSR_N(CSR_SAVE, 13);
+    PRINT_CSR_N(CSR_SAVE, 14);
+    PRINT_CSR_N(CSR_SAVE, 15);
+    PRINT_CSR(CSR_TID);
+    PRINT_CSR(CSR_TCFG);
+    fprintf(f, ".dword  0x%016" PRIx64 " # CSR_TVAL 0x%x\n", \
+                env->timer_counter , 8*idx++);
+    PRINT_CSR(CSR_CNTC);
+    fprintf(f, ".dword  0x%016" PRIx64 " # CSR_TICLR 0x%x\n", \
+                0ul , 8*idx++);
+
+    PRINT_CSR(CSR_LLBCTL);
+
+    PRINT_CSR(CSR_IMPCTL1);
+    PRINT_CSR(CSR_IMPCTL2);
+
+    PRINT_CSR(CSR_TLBRENTRY);
+    PRINT_CSR(CSR_TLBRBADV);
+    PRINT_CSR(CSR_TLBRERA);
+    PRINT_CSR(CSR_TLBRSAVE);
+    PRINT_CSR(CSR_TLBRELO0);
+    PRINT_CSR(CSR_TLBRELO1);
+    PRINT_CSR(CSR_TLBREHI);
+    PRINT_CSR(CSR_TLBRPRMD);
+    PRINT_CSR(CSR_MERRCTL);
+    PRINT_CSR(CSR_MERRINFO1);
+    PRINT_CSR(CSR_MERRINFO2);
+    PRINT_CSR(CSR_MERRENTRY);
+    PRINT_CSR(CSR_MERRERA);
+    PRINT_CSR(CSR_MERRSAVE);
+
+    PRINT_CSR(CSR_CTAG);
+
+    PRINT_CSR_N(CSR_DMW, 0);
+    PRINT_CSR_N(CSR_DMW, 1);
+    PRINT_CSR_N(CSR_DMW, 2);
+    PRINT_CSR_N(CSR_DMW, 3);
+
+    PRINT_CSR(CSR_DBG);
+    PRINT_CSR(CSR_DERA);
+    PRINT_CSR(CSR_DSAVE);
+
+#undef PRINT_CSR
+#undef PRINT_EMPTY_CSR
+
+    // LA_EMU use icount + CSR_CNTC as stable_counter "read out" value
+    fprintf(f, ".dword  0x%016" PRIx64 " # TIMER 0x%x\n",
+            env->icount + env->CSR_CNTC, 8*idx++);
+
+    fclose(f);
+}
+
+void restore_checkpoint_qemu_format(CPULoongArchState *env, char* mem_path, char* cpu_path) {
+
+    fprintf(stderr, "load qemu format ckpt, mem_path=%s,cpu_path=%s\n", mem_path, cpu_path);
+
+    restore_memory_qemu_format(env, mem_path);
+
+    restore_cpu_qemu_format(env, cpu_path);
+
+    // if (env->CSR_CNTC < 0) {
+    //     env->CSR_CNTC = 0;
+    // }
+    env->timer_counter = env->CSR_TVAL;
+    env->CSR_TICLR = 0;
+}
+
+void save_checkpoint_qemu_format(CPULoongArchState *env, char* name) {
+    char filename[1024];
+
+    fprintf(stderr, "qemu format checkpoint at icount:%ld\n", env->icount);
+
+    sprintf(filename, "%s_icount_%ld", name, env->icount);
+
+    if (mkdir(filename, 0755) < 0 && errno != EEXIST) {
+        fprintf(stderr, "ERROR: cannot create dir:%s\n", filename);
+        laemu_exit(1);
+    }
+
+    sprintf(filename, "%s_icount_%ld/mem.bin", name, env->icount);
+    save_memory_qemu_format(env, filename);
+    sprintf(filename, "%s_icount_%ld/cpu.S", name, env->icount);
+    save_cpu_qemu_format(env, filename);
+}
+
 #else
 void save_checkpoint(CPULoongArchState *env, char* name) {
     fprintf(stderr, "error :can not save checkpoint in user mode\n");
 }
 
 void restore_checkpoint(CPULoongArchState *env, char* name) {
+    lsassertm(false, "can not restore checkpoint in user mode\n");
+}
+
+void save_checkpoint_qemu_format(CPULoongArchState *env, char* name) {
+    fprintf(stderr, "error :can not save checkpoint in user mode\n");
+}
+
+void restore_checkpoint_qemu_format(CPULoongArchState *env, char* mem_path, char* cpu_path) {
     lsassertm(false, "can not restore checkpoint in user mode\n");
 }
 #endif
